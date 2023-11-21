d382d751d027ce6e29385ce2711eaeea1834aff2
Jeff Benshetler <jeff.benshetler@gmail.com>
2023-11-21 12:17:37 -0600

    OpenCV now working in Linux.

--- vessel_segmentation.cpp
+++ vessel_segmentation.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
-
+#include <set>
 
 #include <opencv2/opencv.hpp>
+
+void show_image(cv::Mat image, std::string const& title) {
+    cv::imshow(title, image);
+    int key = 0;
+
+    while (key != 27 && key !='q' && key != ' ') {
+        key = cv::waitKey(10) & 0xff;
+    }
+    cv::destroyWindow(title);
+}
 
 struct ExtractArteries {
     ExtractArteries(bool show)
     :
     show_{show}
     {
-        for (auto sz : std::vector<int>{5,11,23} ) {
+        
+        for (auto morph_size : std::vector<int>{2,5,11} ) {
+            auto sz = 2*morph_size + 1;
             structuringElements_.push_back(
                 cv::getStructuringElement( 
                     cv::MORPH_RECT, 
                     cv::Size(sz,sz),
-                    cv::Point(1,1)
+                    cv::Point(morph_size,morph_size)
                 )
             );
         }
@@ -26,9 +38,13 @@ struct ExtractArteries {
 
     bool show() const { return show_; }
 
-    cv::Mat clahe(cv::Mat image) {
+    cv::Mat clahe(cv::Mat image, int channel_index = 0) {
         cv::Mat result;
-        clahe_->apply(image, result);
+        cv::Mat channel;
+        std::cout << "clahe" << std::endl;
+        cv::extractChannel(image, channel, channel_index);
+        std::cout << "channel.size()=" << channel.size() << std::endl;
+        clahe_->apply(channel, result);
         return result;
     }
 
@@ -69,7 +85,7 @@ struct ExtractArteries {
         return f5;
     }
 
-    
+
 
 
 protected:
@@ -81,9 +97,12 @@ protected:
 
 int main(int argc, char* argv[]) {
     auto ex = ExtractArteries(true);
-    auto input_img = cv::imread("drive/DRIVE/test/images/01_test.png");
-    cv::imwrite("output/output.png", ex.clahe(input_img));
-    //cv::imshow("clahe", ex.clahe(input_img) );
+    auto input_img = cv::imread("../drive/DRIVE/test/images/01_test.png");
+    std::cout << input_img.size() << std::endl;
+    cv::imwrite("../output/output.png", input_img);
+    auto clahe = ex.clahe(input_img);
+    std::cout << "clahe.size()=" << clahe.size() << std::endl;
+    show_image(ex.clahe(input_img), "clahe" );
 
     return 0;
 }
